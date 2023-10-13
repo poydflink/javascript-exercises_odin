aa998c425b0f18a5141db2f50f65c3f632184bf4
poydflink <8028391+poydflink@users.noreply.github.com>
2023-10-13 11:27:53 +0100

    Resolution of some basic exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(stringArg, repeatTimes) {
 
+    if (repeatTimes<0)
+    {return "ERROR"}
+    let stringTemp = ""
+    for (let i=0 ;i <repeatTimes; i++ ){
+      
+            stringTemp+=stringArg;
+    }
+
+    return stringTemp
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
